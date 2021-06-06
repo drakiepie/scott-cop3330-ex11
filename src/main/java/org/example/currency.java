28985894e84425b93318faf9e1c0b535f5839e93
Drake Scott <drakescott999@hotmail.com>
2021-06-05 22:13:34 -0400

    copyright

--- src/main/java/org/example/currency.java
+++ src/main/java/org/example/currency.java
@@ -1,21 +1,25 @@
 package org.example;
-
 import java.util.Scanner;
+import java.text.DecimalFormat;
+
+/*
+ *  UCF COP3330 Summer 2021 Exercise 11 Solution
+ *  Copyright 2021 Drake Scott
+ */
 
 public class currency {
+    static DecimalFormat df = new DecimalFormat("$#,##0.00");
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("How many euros are you exchanging? ");
         double amount_from = input.nextDouble();
 
-        System.out.print("What is the current exchange rate in Euros? ");
-        double rate_from = input.nextDouble();
+        System.out.print("What is the exchange rate? ");
+        double rate = input.nextDouble();
 
-        System.out.print("What is the current exchange rate in Dollars? ");
-        double rate_to = input.nextDouble();
+        double finalAmt = amount_from * rate;
 
-        double amount_to = (amount_from * rate_from) / rate_to;
-
-        System.out.println(amount_from + " Euros is " + amount_to + " U.S. Dollars.");
+        System.out.println(amount_from + " Euros at an exchange rate of " + rate + " is " + df.format(finalAmt)
+                + " U.S. Dollars.");
     }
 }
